f85785ee286d928b07d60df47c347a1766803287
AWMKRUNAL <Krunalchulbulkar123@gmail.com>
2026-01-26 20:53:54 +0530

    Z Grid

--- app.py
+++ app.py
@@ -523,7 +523,8 @@ def create_animation_plotly(positions, delays, spacing, burden):
 
     return fig
 
-
+def surface_z (x,y, amplitude = 0.6, freq = 6.0):
+    return amplitude * np.sin(x / freq) * np.cos(y / freq)
 
 def draw_combined_pattern(length, width, spacing, burden, bench_height, pattern_type, hole_details):
     """
@@ -545,7 +546,8 @@ def draw_combined_pattern(length, width, spacing, burden, bench_height, pattern_
 
     x_grid, y_grid = np.meshgrid(x_positions, y_positions)
     #z_grid = np.full_like(x_grid, hole_details["hole_depth"], dtype=float)
-    z_grid = hole_details["hole_depth"] + 0.6 * np.sin(x_grid / 6) * np.cos(y_grid / 6)
+    #z_grid = hole_details["hole_depth"] + 0.6 * np.sin(x_grid / 6) * np.cos(y_grid / 6)
+    z_grid = hole_details["hole_depth"] + surface_z(x_grid, y_grid) 
 
     fig.add_trace(
         go.Surface(
@@ -589,6 +591,7 @@ def add_holes(fig, length, width, spacing, burden, charge_height, stemming_dista
 
 
 def create_hole(x, y, charge_height, stemming_distance, depth_hole):
+    z_surface = surface_z(x,y)
     # Setting marker size to some positive value for the top, and zero for the others
     marker_sizes = [0, 0, 0, 10]
     # Setting marker color as a valid color for the top, and fully transparent for the others
@@ -597,7 +600,8 @@ def create_hole(x, y, charge_height, stemming_distance, depth_hole):
     hole = go.Scatter3d(
         x=[x, x, x, x],
         y=[y, y, y, y],
-        z=[0, charge_height, stemming_distance, depth_hole],
+        #z=[0, charge_height, stemming_distance, depth_hole],
+        z=[z_surface, z_surface + charge_height, z_surface + stemming_distance, z_surface + depth_hole],
         mode="lines+markers",
         line=dict(color=['black', 'grey', 'orange'], width=10),
         marker=dict(color=marker_colors, size=marker_sizes)
@@ -846,6 +850,7 @@ def result():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
